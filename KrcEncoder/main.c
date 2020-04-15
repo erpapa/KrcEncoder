78e81e21a28162f254d3d868183a88e141e79de1
erpapa <hyplcf@163.com>
2020-04-15 08:15:18 +0800

    Initial Commit

---
+++ KrcEncoder/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  KrcEncoder
+//
+//  Created by apple on 2020/4/15.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
